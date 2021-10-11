0c85ab74217c7e02f02a3964758b106c965e4ea4
Anyzooka <80199862+anyzooka1@users.noreply.github.com>
2021-10-11 16:48:30 +0100

    Now reloads archived mods

--- hoi4 launcher/workshopFuncs.cs
+++ hoi4 launcher/workshopFuncs.cs
@@ -8,20 +8,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.IO.Compression;
+using Ionic.Zip;
 
 namespace hoi4_launcher
 {
     public partial class Form1 : Form
     {
-        public string[] replaceDescriptorPath(string[] lines, string path)
+        public string[] replaceDescriptorPath(string[] lines, string path, bool isArchived = false)
         {
             List<string> toRet = new List<string>();
             bool hasPath = false;
-            string myPath = Path.GetFullPath(path).Replace("\\", "/");
 
-            foreach (string line in lines)
+            string[] myLines = lines;
+            string myPath = null;
+
+            if (isArchived)
             {
-                if (contains(line, "path") && !contains(line, "replace_path"))
+                using (ZipFile zip = ZipFile.Read(path))
+                {
+                    ZipEntry e = zip["descriptor.mod"];
+
+                    var x = e.OpenReader();
+
+                    byte[] bytes = new byte[x.Length];
+                    x.Read(bytes, 0, (int)x.Length);
+
+                    myLines = Encoding.Default.GetString(bytes).Split('\n');
+                    myPath = Path.GetFullPath(path).Replace("\\", "/");
+                }
+            }
+            else
+            {
+                myPath = Path.GetFullPath(path).Replace("\\", "/");
+            }
+
+            foreach (string line in myLines)
+            {
+                if (contains(line, "archive") && !contains(line, "replace_path"))
+                {
+                    toRet.Add($"archive=\"{myPath}\"");
+                    hasPath = true;
+                }
+                else if (contains(line, "path") && !contains(line, "replace_path"))
                 {
                     toRet.Add($"path=\"{myPath}\"");
                     hasPath = true;
@@ -54,7 +83,25 @@ namespace hoi4_launcher
                     {
                         // get .mod file
 
-                        string[] newDescriptor = replaceDescriptorPath(File.ReadAllLines(directory + "/descriptor.mod"), directory);
+                        string[] folders = Directory.GetFiles(directory);
+                        bool isArchived = false;
+
+                        string[] newDescriptor = null;
+
+                        foreach (string fName in folders)
+                        {
+                            if (contains(fName, ".zip"))
+                            {
+                                Console.WriteLine("lolol");
+                                isArchived = true;
+                                newDescriptor = replaceDescriptorPath(null, fName, isArchived: true);
+                            }
+                        }
+
+                        if (!isArchived)
+                        {
+                            newDescriptor = replaceDescriptorPath(File.ReadAllLines(directory + "/descriptor.mod"), directory);
+                        }
 
                         File.WriteAllLines($"{folderLocation}/mod/ugc_{directory.Split(Path.DirectorySeparatorChar).Last()}.mod", newDescriptor);
                         count++;
